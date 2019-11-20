f70eb550ae63fcd01d5f1e28d38b92af82a17ff1
Andrei K <1____1@bk.ru>
2019-11-20 18:44:20 +0300

    add Pokemon and PokemonEntity models; add db migrations

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # your models here
+class PokemonEntity(models.Model):
+    lat = models.FloatField(default=0.0)
+    lon = models.FloatField(default=0.0)
+
+
+class Pokemon(models.Model):
+    title = models.TextField()
+    image = models.ImageField(upload_to='pokemons', null=True)
+    coordinate = models.ForeignKey(PokemonEntity, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.title}"
+
+
+
